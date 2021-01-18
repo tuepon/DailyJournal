e036dd8145664c517468d1f500dbff0e5cb9fb7a
Tomonori Ueno <t.uepon@gmail.com>
2021-01-18 14:15:21 +0700

    Create Daily Journal View

--- Daily Journal/EntriesTableViewController.swift
+++ Daily Journal/EntriesTableViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class EntriesTableViewController: UITableViewController {
     
     var entries: [Entry] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,25 +26,33 @@ class EntriesTableViewController: UITableViewController {
             }
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return entries.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        
-        let entry = entries[indexPath.row]
-        
-        cell.textLabel?.text = entry.text
-        
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell") {
+            
+            let entry = entries[indexPath.row]
+            
+            //cell.textLabel?.text = entry.text
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let entry = entries[indexPath.row]
         performSegue(withIdentifier: "segueToEntry", sender: entry)
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let entryVC = segue.destination as? EntryViewController {
             if let entryToBeSent = sender as? Entry {
